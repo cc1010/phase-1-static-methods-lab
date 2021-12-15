e280a44de9c2774fd7383f4fc97f8cfbed818f3c
Chaim Chazan <chaimyisroel@gmail.com>
2021-12-14 21:26:11 -0500

    commit tree 6f22c70304b41b400aa37709c4914d120d47fd2e

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 class Formatter {
-  //add static methods here
+	//add static methods here
+	static capitalize(string) {
+		return string[0].toUpperCase() + string.slice(1);
+	}
+	static sanitize(string) {
+		return string.replace(/[^A-Za-z0-9-'\s]+/g, "");
+	}
+
+	static titleize(string) {
+		const except = [
+			"the",
+			"a",
+			"an",
+			"but",
+			"of",
+			"and",
+			"for",
+			"at",
+			"by",
+			"from",
+		];
+		const arr = string.split(" ");
+		const newArr = [];
+
+		arr.forEach((word) => {
+			if (except.includes(word)) {
+				return newArr.push(word);
+			} else {
+				return newArr.push(this.capitalize(word));
+			}
+		});
+		return this.capitalize(newArr.join(" "));
+	}
 }
